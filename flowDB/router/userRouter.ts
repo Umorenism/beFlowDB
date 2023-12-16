479454349e03acb93bd53f759b3645f87b2664ce
unknown <umorenismvic@gmail.com>
2023-12-16 02:51:51 -0800

    first commit

---
+++ flowDB/router/userRouter.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import {
+  changeUserPassword,
+  createUser,
+  getAllUser,
+  getAllUsers,
+  getCookieUser,
+  logOutUser,
+  resetUserPassword,
+  signinUser,
+  verifyUser,
+} from "../controller/userController";
+import validator from "../utils/validator";
+import { passwordValidator, registerValidator } from "../utils/userValidator";
+import { authorization } from "../utils/authorization";
+import { authRized } from "../utils/authorized";
+
+const router: Router = Router();
+router.route("/register-user").post(validator(registerValidator), createUser);
+router.route("/sign-in-user").post(signinUser);
+
+router.route("/all-users").get(authorization, getAllUsers);
+
+router.route("/all-user").get(authRized, getAllUser);
+
+router.route("/verify-user").patch(verifyUser);
+router.route("/logout").get(logOutUser);
+router.route("/user-cookie").get(getCookieUser);
+
+router.route("/reset-user-password").patch(resetUserPassword);
+router
+  .route("/change-user-password/:userID")
+  .patch(validator(passwordValidator), changeUserPassword);
+export default router;
---
+++ flowDB/utils/enums.ts
@@ -0,0 +1,16 @@
+export enum HTTP {
+  OK = 200,
+  CREATED = 201,
+  BAD = 404,
+}
+export enum SCHOOL {
+  ADMIN = "admin",
+  TEACHER = "teacher",
+  STUDENT = "student",
+}
+export enum FOOD {
+  ADMIN = "admin",
+  CLIENT = "client",
+  DISPATCHER = "dispatcher",
+  VENDOR = "vendor",
+}
